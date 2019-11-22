2fcc1c902872dae16e0b95b75ba283f4b879bb11
Tominano <martontamas@caesar.elte.hu>
2019-11-22 14:00:27 +0100

    new

---
+++ beadando/szovegstat/szovegstat.cpp
@@ -0,0 +1,17 @@
+/*
+Szöveg statisztika
+Készíts egy osztályt TextStat néven, amely egy szöveg szavait kezeli, és statisztikát ad róluk.
+Az osztály az alábbi tagfüggvényekkel rendelkezik:
+
+- add(std::string): Új szó hozzáadása a gyűjteményhez.
+- size(): Megadja a szavak számát.
+- print(): Kiírja a szavakat.
+- getLongest(): Megadja a gyűjtemény leghosszabb szavát.
+- getShortest(): Megadja a gyűjtemény legrövidebb szavát.
+- longerThan(int): Megadja az n karakternél hosszabb szavakat.
+- numUnique(): Megadja az egyedi szavak számát.
+            Például: alma, korte, alma, narancs, narancs, alma -> 3.
+- mostFrequent(): Megadja a gyűjtemény leggyakoribb szavát.
+            Például: alma, korte, alma, narancs, narancs, alma -> alma.
+- Olvass be néhány szót a standard bemenetről vagy fájlból, és helyezd el egy TextStat objektumban.
+            Hívd meg a fenti függvényeket a teszteléshez.*/
